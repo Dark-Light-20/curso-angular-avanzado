9dab8b0c877e3f443cfbc15db43e10595514f1fb
Dark-Light-20 <pazsebas@hotmail.com>
2025-07-12 13:54:22 -0500

    build: :construction_worker: Added environment config

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  prduction: false,
+  apiUrl: 'https://api.escuelajs.co', // TODO: Validate local api server
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  prduction: true,
+  apiUrl: 'https://api.escuelajs.co',
+};
