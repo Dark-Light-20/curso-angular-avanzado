124664d1668f1f9cabb959adc9d394cc3a684e5b
Dark Light <pazsebas@hotmail.com>
2025-11-07 17:09:08 +0000

    feat: :sparkles: Implemented leafleftjs to add a map for stores locations and current origin location

--- src/app/domains/info/pages/locations/locations.component.ts
+++ src/app/domains/info/pages/locations/locations.component.ts
@@ -1,11 +1,14 @@
 import {
   afterNextRender,
   Component,
+  effect,
   inject,
   resource,
   signal,
 } from '@angular/core';
 import { LocationsService } from '@shared/services/locations.service';
+import { Marker } from 'leaflet';
+import { Location } from '@shared/models/location.model';
 
 @Component({
   selector: 'app-locations',
@@ -15,6 +18,10 @@ export default class LocationsComponent {
   private readonly _locationsService = inject(LocationsService);
 
   readonly $origin = signal('');
+  readonly $originMarker = signal<Marker | null>(null);
+  readonly $markers = signal<Marker[]>([]);
+
+  map: L.Map | null = null;
 
   locationsResource = resource({
     request: () => ({ origin: this.$origin() }),
@@ -25,6 +32,18 @@ export default class LocationsComponent {
   constructor() {
     afterNextRender(() => {
       this.getCurrentLocation();
+      this.initializeMap();
+    });
+
+    effect(() => {
+      const origin = this.$origin();
+      const locations = this.locationsResource.value();
+      if (!origin || !locations) {
+        return;
+      }
+      const [lat, lng] = origin.split(',').map(Number);
+      this.addStoreMarkers(locations);
+      this.updateMapLocation(lat, lng);
     });
   }
 
@@ -34,5 +53,43 @@ export default class LocationsComponent {
         `${position.coords.latitude},${position.coords.longitude}`
       );
     });
+  }
+
+  async initializeMap(): Promise<void> {
+    const L = await import('leaflet');
+    this.map = L.map('map').setView([51.505, -0.09], 13);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; OpenStreetMap contributors',
+    }).addTo(this.map);
+  }
+
+  async updateMapLocation(lat: number, lng: number): Promise<void> {
+    if (this.map) {
+      const L = await import('leaflet');
+      const originMarker = this.$originMarker();
+      if (originMarker) {
+        originMarker.remove();
+      }
+      this.$originMarker.set(null);
+      this.map.setView([lat, lng], 12);
+      const marker = L.marker([lat, lng], {
+        icon: L.icon({ iconUrl: 'assets/loc-pin.svg' }),
+      }).addTo(this.map);
+      this.$originMarker.set(marker);
+    }
+  }
+
+  async addStoreMarkers(locations: Location[]): Promise<void> {
+    if (this.map) {
+      const L = await import('leaflet');
+      this.$markers().forEach(marker => marker.remove());
+      this.$markers.set(
+        locations.map(location =>
+          L.marker([location.latitude, location.longitude], {
+            icon: L.icon({ iconUrl: 'assets/map-pin.svg' }),
+          }).addTo(this.map!)
+        )
+      );
+    }
   }
 }
